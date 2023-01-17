3dd83e06c6f9898efc098353414b513f2a60428a
Oseremhen <106120403+Oseremhen@users.noreply.github.com>
2023-01-17 05:53:53 -0500

    updates

--- EmployeeSQL/Data_Engineering.sql
+++ EmployeeSQL/Data_Engineering.sql
@@ -8,36 +8,38 @@ DROP TABLE IF EXISTS salaries;
 
 --Create all the tables
 CREATE TABLE departments(
-	dept_no VARCHAR PRIMARY KEY NOT NULL,
-	dept_name VARCHAR NOT NULL
+	dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE titles(
-	title_id VARCHAR PRIMARY KEY NOT NULL,
-	title VARCHAR NOT NULL
+	title_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	title VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employees(
 	emp_no INT PRIMARY KEY NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
+    emp_title_id VARCHAR(10) NOT NULL,
     birth_date DATE,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
     hire_date DATE,
     FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
-	dept_no VARCHAR NOT NULL,
-	emp_no INT PRIMARY KEY NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
